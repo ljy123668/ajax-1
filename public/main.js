eb2524d95263cbff438ea23e59187f0645db9631
赵伟棋 <651507008@qq.com>
2020-12-17 11:56:32 +0800

    增加

---
+++ public/main.js
@@ -0,0 +1,112 @@
+console.log('我是main.js 2 ');
+
+let n = 1
+getPage.onclick = () => {
+  console.log(1);
+  const request = new XMLHttpRequest()
+  request.open('GET', `/page${n+1}`)
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const array = JSON.parse(request.response)
+      array.forEach(item => {
+        const li = document.createElement('li')
+        li.textContent = item.id
+        xxx.appendChild(li)
+      });
+      n += 1
+
+    }
+
+  }
+  request.send()
+}
+
+
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest()
+  request.open('GET', '/5.json')
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const object = JSON.parse(request.response)
+      myName.textContent = object.name
+    }
+
+  }
+  request.send()
+
+}
+
+getXML.onclick = () => {
+  const request = new XMLHttpRequest()
+  request.open('GET', '/4.xml')
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+
+      const dom = request.responseXML;
+      const text = dom.getElementsByTagName('warning')[0].textContent
+      console.log(text.trim());
+    }
+  }
+  request.send()
+
+}
+getHTML.onclick = () => {
+  const request = new XMLHttpRequest()
+  request.open('GET', '/3.html')
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const div = document.createElement('div')
+        div.innerHTML = request.response
+        document.body.appendChild(div)
+      } else {
+        alert('加载失败')
+      }
+
+    }
+  }
+
+  // request.onload = () => {
+  //   const div = document.createElement('div')
+  //   div.innerHTML = request.response
+  //   document.body.appendChild(div)
+  // }
+  // request.onerror = () => {
+
+  // }
+  request.send()
+}
+
+getJS.onclick = () => {
+  const request = new XMLHttpRequest()
+  request.open('GET', '/2.js')
+  request.onload = () => {
+    console.log(request.response)
+    const script = document.createElement('script')
+    script.innerHTML = request.response
+    document.body.appendChild(script)
+  }
+  request.onerror = () => {}
+  request.send()
+
+
+}
+
+getCSS.onclick = () => {
+
+  const request = new XMLHttpRequest()
+  request.open('GET', '/style.css')
+  request.onload = () => {
+    console.log('request.response');
+    console.log(request.response);
+
+    const style = document.createElement('style')
+    style.innerHTML = request.response
+    document.head.appendChild(style)
+  }
+  request.onerror = () => {
+    console.log('失败了');
+  }
+  request.send()
+}
